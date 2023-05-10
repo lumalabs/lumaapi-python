137eea645563139f95a40bde206ecdb47386ce32
Alex Yu <alexyu99126@gmail.com>
2023-05-10 00:17:45 -0700

    Add docs

---
+++ docs/source/conf.py
@@ -0,0 +1,79 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+import subprocess
+import sphinx_rtd_theme
+sys.path.insert(0, os.path.join(os.path.abspath('.'), '..'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'lumaapi'
+copyright = '2023, Luma AI'
+author = 'Luma AI'
+
+# The full version, including alpha/beta/rc tags
+
+release = subprocess.check_output(sys.executable + " -m pip show lumaapi | grep Version | cut -d ' ' -f 2",
+                                  shell=True).decode('utf-8').strip()
+print('lumaapi', release)
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+        'recommonmark',
+        'sphinx.ext.autodoc',
+        'sphinx_rtd_theme',
+    ]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = []
+
+add_module_names = False
+
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
+
+autodoc_member_order = 'bysource'
+
+html_theme_options = {
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,45 @@
+.. lumaapi documentation master file, created by
+   sphinx-quickstart on Wed Sep 15 10:48:24 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Luma API Python + CLI Client Reference
+=========================================================
+
+This is a Python and CLI client for the `Luma API <https://lumalabs.ai/luma-api>`.
+
+* Installation: :code:`pip install lumaapi`
+* Python library reference: :ref:`lumaapi`.
+* Luma API reference: https://documenter.getpostman.com/view/24305418/2s93CRMCas
+
+Example CLI usage
+*****************************
+
+.. code-block:: shell
+
+    # Check credits
+    luma credits
+    # Submit video, zip, or folder (of images). Prints slug
+    luma submit <path> <title>
+    # Check status of slug
+    luma status <slug>
+
+
+Example usage inside Python
+*****************************
+
+.. code-block:: python
+
+    from lumaapi import LumaClient
+    client = LumaClient(api_key)
+    slug = client.submit(video_path, title)
+    print(client.status(slug))
+
+
+Please see the reference for more details
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   lumaapi
